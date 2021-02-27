4767299f119a85be0b9f5e01a720d79e7a55b2a6
Vadim <65212516+VadimChorrny@users.noreply.github.com>
2021-02-27 10:04:07 +0200

    add new task

--- Facade/Translater.cpp
+++ Facade/Translater.cpp
@@ -1,1 +1,59 @@
 #include "Translater.h"
+#include <list>
+#include <iostream>
+using namespace std;
+
+class Word
+{
+	string en, ua;
+public:
+	Word(string& en, string ua) : en(en), ua(ua) {}
+};
+
+class CollectionWord : public Word
+{
+private:
+	list<Word> words;
+	list<Word>::iterator it;
+public:
+	void AddWord(Word w)
+	{
+		this->words.push_back(w);
+	}
+	void DeleteWord(string s)
+	{
+		it = find(begin(words), end(words), s);
+		if (it != end(words))
+		{
+			words.erase(it);
+		}
+		else
+		{
+			cerr << "Words dont find!" << endl;
+		}
+	}
+	void ShowAllWords()
+	{
+		for (auto& i : words)
+		{
+			i;
+		}
+	}
+};
+
+class Facade : public Word
+{
+private:
+	CollectionWord* words_ = nullptr;
+public:
+	Facade(CollectionWord* words) :words_(words) {}
+	~Facade() { delete words_; }
+
+	string Operation()
+	{
+	}
+
+
+
+
+};
--- Factory_method/Source.cpp
+++ Factory_method/Source.cpp
@@ -4,10 +4,13 @@ using namespace std;
 
 int main()
 {
-	Dialog* dialog = new WindowsDialog();
-	ClientCode(*dialog);
-	cout << "________" << endl;
-	Dialog* dialog_two = new HTMLDialog();
-	ClientCode(*dialog_two);
-
+	//Dialog* dialog = new WindowsDialog();
+	//ClientCode(*dialog);
+	//cout << "________" << endl;
+	//Dialog* dialog_two = new HTMLDialog();
+	//ClientCode(*dialog_two);
+	int i;
+	for (i = 0; i < 5; i++)
+		cout << i;
+	cout << i;
 }
